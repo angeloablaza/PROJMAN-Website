28f38d3c5884541e2ba68e67c23f97fc665be140
angeloablaza <40690315+angeloablaza@users.noreply.github.com>
2018-10-15 19:59:04 +0800

    heroku time

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const path = require("path")
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, "public"))) //so we can access outside folders
+
+app.get("/", (req, res)=>{
+    res.sendFile(path.join(__dirname, "index.html"))
+    // res.sendFile(__dirname + "/index.html")
+})
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("listening at port 3000")
+})
