81c0fbc1bff2c977ebca4a9dc09932bf1a486a87
angeloablaza <40690315+angeloablaza@users.noreply.github.com>
2018-11-12 20:59:17 +0800

    progress report 3

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ const path = require("path")
 
 const app = express()
 
-app.use(express.static(path.join(__dirname, "public"))) //so we can access outside folders
+//app.use(express.static(path.join(__dirname, "public"))) //so we can access outside folders
+app.use(express.static(path.join(__dirname))) //so we can access outside folders
 
 app.get("/", (req, res)=>{
     res.sendFile(path.join(__dirname, "index.html"))
+    // res.sendFile(__dirname + "/index.html")
+})
+
+app.get("/about", (req, res)=>{
+    res.sendFile(path.join(__dirname, "about.html"))
     // res.sendFile(__dirname + "/index.html")
 })
 
